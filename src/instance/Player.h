43e804ea158a283f72a37ad5bcc0ba0d257a5efb
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-04-08 20:39:35 +0200

    Explained the problem

--- src/instance/Player.h
+++ src/instance/Player.h
@@ -21,6 +21,22 @@ class Player: public Entity {
                 dy += 16.0f;
             }
 
+            /* TODO
+             *
+             * Hmm... We need to have the World class / object
+             * (the current world that this player exists in) in scope.
+             *
+             * How would we achieve this?
+             * this is needed to be able to check for collisions.
+             * We currently cannot reach the current world object from here.
+             *
+             * something like:
+             *
+             *     getCurrentWorld().getCurrentChunk(this).getBlocks();
+             *
+             * would have been very nice.
+             */
+
             updatePhysics(delta); 
         }
 
@@ -29,9 +45,9 @@ class Player: public Entity {
             glColor3f(1.0f, 0.0f, 0.0f);
             glPushMatrix();
             glTranslatef(x, y, -1.0f);
-            
+
             glBegin(GL_LINE_LOOP);
-            
+
             glVertex2f(0.0f, 0.0f);
             glVertex2f(this->w, 0.0f);
             glVertex2f(this->w, this->h);
