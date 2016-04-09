c5098cae248965c8c1eb3c54837024fbe65cc94e
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-04-09 21:57:40 +0200

    solved problems with collisions and chunks

--- src/instance/Player.h
+++ src/instance/Player.h
@@ -15,6 +15,10 @@ class Player: public Entity {
         void tick (float delta) {
             updatePhysics(delta);
 
+            int chunkx = (int)((int)(x+(dx*delta))%((16*16)*100))/(16*16);
+            int chunky = (int)((int)(y+(dy*delta))%((16*16)*8))/(16*16);
+            Chunk * current_chunk = world->chunks[chunkx][chunky];
+
             if (state[SDL_SCANCODE_LEFT]) {
                 dx -= 16.0f;
             }
@@ -34,27 +38,39 @@ class Player: public Entity {
             }
             
             on_ground = false;
-            Chunk * chunk = world->chunks[(int)((int)(x+(dx*delta))%((16*16)*100))/(16*16)][(int)((int)(y+(dy*delta))%((16*16)*8))/(16*16)];
-            for (int xx = 0; xx < sizeof(chunk->blocks)/(sizeof(*chunk->blocks)); xx++) {
-                for (int yy = 0; yy < sizeof(chunk->blocks[xx])/sizeof(*chunk->blocks[xx]); yy++) {
-                    Block * block = chunk->blocks[xx][yy];
-                    if (block->type == &BLOCK_AIR) { continue; }
 
-                    int inst_x = block->x;
-                    int inst_y = block->y;
-                    int inst_w = block->w;
-                    int inst_h = block->h;
-                    
-                    if ((y+h) + (dy * delta) >= inst_y && y+(dy * delta) <= inst_y+inst_h) {
-                        if ((x+w)+(dx * delta) >= inst_x && x+(dx * delta) <= inst_x+inst_w) {
-                            
-                            if (!(x+w+(dx*delta) < block->x && x+(dx*delta) > block->x+block->w)) {
-                                dy -= dy;
-                                on_ground = true;
-                            }
-                            
-                            if (!(y+h+(dy*delta) < block->y)) {
-                                dx -= dx;
+            Chunk * chunks[] = {
+                current_chunk,
+                world->chunks[max(0, chunkx-1)][chunky],
+                world->chunks[chunkx+1][chunky],
+                world->chunks[chunkx][chunky+1],
+                world->chunks[chunkx][max(0, chunky-1)]
+
+            };
+
+            for (int cc = 0; cc < (sizeof(chunks)/sizeof(*chunks)); cc++) {
+                Chunk * chunk = chunks[cc];
+                for (int xx = 0; xx < sizeof(chunk->blocks)/(sizeof(*chunk->blocks)); xx++) {
+                    for (int yy = 0; yy < sizeof(chunk->blocks[xx])/sizeof(*chunk->blocks[xx]); yy++) {
+                        Block * block = chunk->blocks[xx][yy];
+                        if (block->type == &BLOCK_AIR) { continue; }
+
+                        int inst_x = block->x;
+                        int inst_y = block->y;
+                        int inst_w = block->w;
+                        int inst_h = block->h;
+                        
+                        if ((y+h) + (dy * delta) >= inst_y && y+(dy * delta) <= inst_y+inst_h) {
+                            if ((x+w)+(dx * delta) >= inst_x && x+(dx * delta) <= inst_x+inst_w) {
+                                
+                                if (!(x+w-(dx*delta) < block->x || x-(dx*delta) > block->x+block->w)) {
+                                    dy -= dy;
+                                    on_ground = true;
+                                }
+                                
+                                if (!(y+h+(dy*delta) < block->y+0.1f)) {
+                                    dx -= dx;
+                                }
                             }
                         }
                     }
